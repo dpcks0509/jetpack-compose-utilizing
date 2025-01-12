aea4e38616fa4691b00f05c212f03c676cb3a2e8
dpcks0509 <dpcks0509@gmail.com>
2025-01-12 20:02:20 +0900

    feat: DropDownMenu

--- part4-chapter4-10/app/src/main/java/kr/co/fastcampus/part4/chapter4_10/MainActivity.kt
+++ part4-chapter4-10/app/src/main/java/kr/co/fastcampus/part4/chapter4_10/MainActivity.kt
@@ -5,8 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Button
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import kr.co.fastcampus.part4.chapter4_10.ui.theme.DropDownMenuTheme
@@ -43,6 +52,23 @@ fun DropDownMenuEx() {
     // 단계 1: `DropdownMenu`를 만들고 `expanded`를 `expandDropDownMenu`로
     // 등록합시다.
     // `onDismissRequest`에 대해서는 `expandDropDownMenu`를 `false`로 바꿉니다.
+    DropdownMenu(
+        expanded = expandDropDownMenu,
+        onDismissRequest = {
+            expandDropDownMenu = false
+        }
+    ) {
+        DropdownMenuItem(onClick = {
+            counter++
+        }) {
+            Text("증가")
+        }
+        DropdownMenuItem(onClick = {
+            counter--
+        }) {
+            Text("감소")
+        }
+    }
 
     // 단계 2: 두개의 `DropdownMenuItem`을 등록합시다. `onClick`을 구현하고
     // 내용물은 `Text`로 채워봅시다.
